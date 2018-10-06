c55c7ad746e70143525433ac87a7e2c792f8627b
Ivan Chekhovskikh <chekhovskikh@mail.ru>
2018-10-07 00:34:34 +0400

    added food info

--- app/src/main/java/com/sugar/steptofood/FoodInfoActivity.kt
+++ app/src/main/java/com/sugar/steptofood/FoodInfoActivity.kt
@@ -8,5 +8,6 @@ class FoodInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_info)
+        getSupportActionBar()?.hide()
     }
 }
